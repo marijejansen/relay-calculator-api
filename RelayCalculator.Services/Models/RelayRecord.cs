48edc9c4f824695b3a18aaa85d6fbddf19cf8df9
Marije Jansen <marije.jansen@winvision.nl>
2020-05-30 10:35:58 +0200

    wop pdf reader

---
+++ RelayCalculator.Services/Models/RelayRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelayCalculator.Services.Models
+{
+    using RelayCalculator.Services.Enums;
+
+    public class RelayRecord
+    {
+        public double Time
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public Relay Relay
+        {
+            get;
+            set;
+        }
+
+        public string Area
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ RelayCalculator.Services/RecordsService.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RecordsService.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the RecordsService type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RelayCalculator.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using iTextSharp.text.pdf;
+    using iTextSharp.text.pdf.parser;
+    using RelayCalculator.Services.Models;
+
+    public class RecordsService
+    {
+        public IEnumerable<RelayRecord> GetRelayRecords()
+        {
+
+            return new List<RelayRecord>();
+        }
+
+        private string GetPdfContent()
+        {
+            var fileNameLongCourse = "- overzicht van WMR, EMR en NMR lange baan";
+            var reader = new PdfReader($"@{fileNameLongCourse}");
+
+            return "NEE";
+        }
+    }
+}
